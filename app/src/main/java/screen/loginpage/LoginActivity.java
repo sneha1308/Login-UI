b6713dc7d4e3cd44d6fe6f74f28770c0e42bfc48
Sneha <sneha.gangireddy@smartron.com>
2017-11-30 19:01:00 +0530

    login page

---
+++ app/src/main/java/screen/loginpage/LoginActivity.java
@@ -0,0 +1,17 @@
+package screen.loginpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
+import android.widget.ViewFlipper;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
